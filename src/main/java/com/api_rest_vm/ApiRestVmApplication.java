9b17f72fe79fa1c7b7868bf2811938ceea6893d5
Aijalon <aijalon.ti@gmail.com>
2025-03-28 23:05:34 -0300

    feat(vm): iniciando projeto

---
+++ src/main/java/com/api_rest_vm/ApiRestVmApplication.java
@@ -0,0 +1,13 @@
+package com.api_rest_vm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestVmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestVmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_rest_vm/ApiRestVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_rest_vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestVmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
